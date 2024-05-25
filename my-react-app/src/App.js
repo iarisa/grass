16a7aae4b6241ce79e97e8615f28814190b094b0
stellurion <sarah.shi.marlboro@gmail.com>
2024-05-25 15:09:42 -0700

    BUTTON

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,20 +6,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyButton />
       </header>
     </div>
   );
 }
-
 export default App;
+
+function MyButton() {
+  let latitude;
+  let longitude;
+
+  function requestLocation() {
+    if(navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+      position => { ({latitude, longitude} = position.coords); console.log(`Latitude: ${latitude}, Longitude: ${longitude}`); });
+  }
+
+  return (
+    <button onClick={requestLocation}>
+        Location
+    </button>
+  );
+}
